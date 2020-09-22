1794bcc790f9440b20ead2c2224b60f5034f7ddf
Anup Bagali <anup.bagali1@gmail.com>
2020-09-22 13:09:46 -0400

    lerps

--- mandelbrot.c
+++ mandelbrot.c
@@ -7,9 +7,9 @@
 #define X 1920
 #define Y 1080
 
-#define R_MAX 2.0
-#define R_MIN -R_MAX
-#define I_MAX 1.5
+#define R_MAX 1.5
+#define R_MIN -2
+#define I_MAX 1.0
 #define I_MIN -I_MAX
 
 #define MAX_ITER 8000
@@ -20,15 +20,19 @@ typedef struct {
     int b;
 } rgb;
 
+double lerp(double v0, double v1, double t) {
+    return (1 - t) * v0 + t * v1;
+}
 
-int mandelbrot(int px, int py){
+
+rgb mandelbrot(int px, int py, rgb* palatte){
     double x = 0; // complex (c)
     double y = 0;
 
     double x0 = R_MIN + (px * ((R_MAX - R_MIN)/(X*1.0))); // complex scale of Px
     double y0 = I_MIN + (py * ((I_MAX - I_MIN)/(Y*1.0))); // complex scale of Py
 
-    int i = 0;
+    double i = 0;
   
     while(x*x + y*y <= 20 && i < MAX_ITER){
         double xtemp = x*x - y*y + x0;
@@ -36,13 +40,26 @@ int mandelbrot(int px, int py){
         x = xtemp;
         i++;
     }
-    return i;
-    /* WIP big brain log coloring 
     if(i < MAX_ITER){
-        log_zn = log(x*x + y*y) / 2;
-        nu = log(log_zn / log(2))/log(2);
+        double log_zn = log(x*x + y*y) / 2;
+        double nu = log(log_zn / log(2))/log(2);
+        i += 1 - nu;
     }
-    */
+    rgb c1 = palatte[(int)i];
+    rgb c2;
+    if((int)i + 1 > MAX_ITER){
+        c2 = palatte[(int)i];
+    }else{
+        c2 = palatte[((int)i)+1];
+    }
+
+    double mod = ((int)i) - i; // cant mod doubles
+    return (rgb){
+        .r = (int)lerp(c1.r, c2.r, mod),
+        .g = (int)lerp(c1.g, c2.g, mod),
+        .b = (int)lerp(c1.b, c2.b, mod),
+    };
+
 }
 
 
@@ -59,6 +76,10 @@ int main(){
             continue;
         }
         double j = 3.0 * (i == 0 ? 0:log(i))/log(MAX_ITER-1.0);
+
+        if (j<0){
+            j *= -1;
+        }
 
         if (j<1){
             palatte[i] = (rgb){
@@ -85,8 +106,7 @@ int main(){
     printf("finished palatte\n");
     for(int Py = 0; Py < Y; Py++){
         for(int Px = 0; Px < X; Px++){
-            int i = mandelbrot(Px, Py);
-            colors[Py][Px] = palatte[i];
+            colors[Py][Px] = mandelbrot(Px, Py, palatte);
         }
     }
     printf("finished calcs\n");
@@ -97,7 +117,7 @@ int main(){
     fprintf(fout, "255\n");
     for(int y = 0; y < Y; y++){
         for(int x = 0; x < X; x++){
-            fprintf(fout, "%d %d %d\n", colors[y][x].r, colors[y][x].g, colors[y][x].b);
+            fprintf(fout, "%ld %ld %ld\n", (int)colors[y][x].r, (int)colors[y][x].g, (int)colors[y][x].b);
         }
     }
 }
